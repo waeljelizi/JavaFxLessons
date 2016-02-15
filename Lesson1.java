d53b5fd6ff1b743b418655b2bf011ebd82613a7e
waeljelizi <neowael2008@gmail.com>
2016-02-15 22:21:50 +0100

    Update Lesson1.java

--- Lesson1.java
+++ Lesson1.java
@@ -1,8 +1,6 @@
-package javafxapplication1;
+package javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -10,25 +8,34 @@ import javafx.stage.Stage;
 
 
 public class Lesson1 extends Application {
-  
+    
+    //Stage means the window
+    Stage window;
+    
     @Override
     public void start(Stage primaryStage) {
-        
+        window = primaryStage;
+        //creating a label to display hello world message
         Label text = new Label();
         text.setText("Hello World");
-       
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //creating a layout [StackPane make any item at the center of window]
+        StackPane layout = new StackPane();
+        //adding the text to the layout
+        layout.getChildren().add(text);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Scene means the content of the window
+        Scene scene = new Scene(layout, 300, 250);
+        //setting window's title
+        window.setTitle("Hello World!");
+        //adding the content to the window
+        window.setScene(scene);
+        //display the window
+        window.show();
     }
     
     public static void main(String[] args) {
+        //launch method is used to start our application
         launch(args);
     }
     
