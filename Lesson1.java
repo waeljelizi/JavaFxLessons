702e289ea2823919bcf0ba2abaaedde61f661bdc
waeljelizi <neowael2008@gmail.com>
2016-02-15 22:12:15 +0100

    Create Lesson1.java

---
+++ Lesson1.java
@@ -0,0 +1,35 @@
+package javafxapplication1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Lesson1 extends Application {
+  
+    @Override
+    public void start(Stage primaryStage) {
+        
+        Label text = new Label();
+        text.setText("Hello World");
+       
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
